2661c805b17f0edd2d1281b0ac7ca417add2fe9f
vaideshshank <vaideshshanky@gmail.com>
2019-03-19 18:30:21 +0530

    push

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,21 +7,18 @@ route=(obj)=>{
 function parallax1(el,scrollVal){
     $(el).css('margin-top',-scrollVal/70+"%");
 }
-
+var i=0,curr_scr=this.scrollLeft;
 $(document).ready(function(){
     
     var i=false;
-    //slide menu
-    /*$("#navigation>i").click(function(){
-        if(i){
-            $(".mobile-menu").css({
-                width:0
-            })
-        }
-        
-    })*/
-    //var topPos=$("#section3 .iframes").offset().top;
+    
+    var scrollPos=[]
+    for(i=0; i<7; i++){
+        scrollPos.push($("#section3 .iframes>section").eq(i).offset().left+10);
+    }
+    
     $("#section3 .iframes").mousewheel(function(e,delta){
+         
         if(this.scrollLeft!=this.scrollLeft-delta*5){
             var s1=this.scrollLeft;
             this.scrollLeft-=delta*5;
@@ -30,7 +27,44 @@ $(document).ready(function(){
             }
             
         }
+      //  curr_scr=this.scrollLeft;   
     })
+
+    //scroll Animation
+    var i=0;
+
+    function scrollAnimate(target,speed){
+        setInterval(function(){
+            if(i==6){
+                $(target).animate({scrollLeft:scrollPos[0]-scrollPos[i]},{
+                    easing:'swing',
+                    duration:speed,
+                    complete:function(){
+                        i=(++i)%7;
+                        //console.log(i);
+                    }
+                });    
+            }else{
+            $(target).animate({scrollLeft:scrollPos[i+1]-scrollPos[0]},{
+                duration:speed,
+                complete:function(){
+                    i=(++i)%7;
+                    //console.log(i);
+                }
+            });
+        }
+            
+        },speed+1500);
+    }
+    
+    /*$("#section3 .iframes>section").animate({
+        'position':'relative',
+        'left':'-=100px'
+    },200);*/
+
+    scrollAnimate($("#section3 .iframes"),3000);
+            
+    
     
     $(window).scroll(function(e){
         var scroll=$(this).scrollTop();
