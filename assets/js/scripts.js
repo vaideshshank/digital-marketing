4e8800f152e939b57cd840eef3714584b86de6aa
vaideshshank <vaideshshanky@gmail.com>
2019-03-21 00:06:23 +0530

    scroll mods

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,6 @@
-route=(obj)=>{
+"use strict"
+
+function route(obj){
     var page=obj.innerHTML;
     page=page.toLowerCase();
     window.location+='templates/'+page+'.html';
@@ -7,29 +9,43 @@ route=(obj)=>{
 function parallax1(el,scrollVal){
     $(el).css('margin-top',-scrollVal/70+"%");
 }
+
 var pos=0,curr_scr=this.scrollLeft;
+
+
 $(document).ready(function(){
-    
+
+    //scroll variables
+    let iframeTop=$("#section3 .iframes").offset().top,
+        scrollDown=false,scrollUp=false;
     var check=false;
-    
-    var scrollPos=[]
+    var scrollPos=[];
+
     for(i=0; i<7; i++){
         scrollPos.push($("#section3 .iframes>section").eq(i).offset().left+10);
     }   
     
+    $(window).resize(function(){
+        scrollPos=[];
+        for(i=0; i<7; i++){
+            scrollPos.push($("#section3 .iframes>section").eq(i).offset().left+10);
+        }   
+    })
+
+    //Iframe slider animation
 
     $("#section3 .iframes").mousewheel(function(e,delta){
         var s1=this.scrollLeft;
-                this.scrollLeft-=delta;
+        this.scrollLeft-=delta;
                 
         if(this.scrollLeft-s1!=0){
             e.preventDefault();
         }
+
+        if(pos==6){console.log(this.scrollLeft-s1);}
+
         if(!check){
             check=true;
-            
-                
-                
                 if(pos<6 && (this.scrollLeft)-s1>0){
                     (this).scrollLeft-=delta*5;
                     pos=(++pos);
@@ -38,7 +54,7 @@ $(document).ready(function(){
                     pos=(--pos);
                  }
                 
-                console.log(pos);
+                //console.log(pos);
    
                 $(this).animate({scrollLeft:scrollPos[pos]-scrollPos[0]},{
                     easing:'swing',
@@ -46,35 +62,39 @@ $(document).ready(function(){
                     complete:function(){
                         check=false;
                     }
-                });
-            
-            
-        }
-        
-        
-        
-      //  curr_scr=this.scrollLeft;   
+                });   
+        } 
     })
 
-    //scroll Animation
     var i=0;
+    //Parallax effect
 
-    
-    /*$("#section3 .iframes>section").animate({
-        'position':'relative',
-        'left':'-=100px'
-    },200);*/
-
-    //scrollAnimate($("#section3 .iframes"),3000);
-            
-    
-    
     $(window).scroll(function(e){
+
+        //parallax effect
         var scroll=$(this).scrollTop();
         parallax1("#dots3",scroll);
 
         if((scroll+$(this).height())>$("#dots5").offset().top-4){
             parallax1("#dots5",((scroll+$(this).height())-$("#dots5").offset().top-20));
         }
+
+
+        /*if(scroll>iframeTop-150 && !scrollDown){
+            $("html, body").animate({scrollTop:iframeTop},{
+                easing:'swing',
+                duration:1000,
+                complete:function(){scrollDown=!scrollDown;}
+            });   
+        }else if(scroll>iframeTop-150 && !scrollUp){
+            $("html, body").animate({scrollTop:iframeTop},{
+                easing:'swing',
+                duration:1000,
+                complete:function(){scrollUp=!scrollUp;}
+            });  
+        }*/
+        
+        
     })
+    
 })
